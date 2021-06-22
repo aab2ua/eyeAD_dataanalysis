d3810f8eb71fb5abfc45c4fd115a65d19b116601
tarsus-esurgi <85324208+tarsus-esurgi@users.noreply.github.com>
2021-06-21 18:49:48 -0700

    Update EyeAD_algorithm.R
    added dispersion and velocity algorithms

--- EyeAD_algorithm.R
+++ EyeAD_algorithm.R
@@ -43,6 +43,7 @@ for (i in 1:length(event_folders)) {
       mutate(trial = 1)%>%
       mutate(distance = 1000)
     
+    #GAZEPATH
     alg_results_gaze = gazepath(eye_data, x1 = 8, y1 = 9, d1 = 12, trial = 11, height_mm = screen[1,2],
                                 width_mm = screen[2,2], width_px = screen[3,2], height_px = screen[4,2], method = "gazepath",
                                 samplerate = 1000, thres_dur = 80)
@@ -53,23 +54,71 @@ for (i in 1:length(event_folders)) {
     #}
     
     #Getting fixation metrics:
-    fix_filtered = s_gaze%>%
+    fix_filtered_gaze = s_gaze%>%
       filter(Value == "f")
     
-    fix_count_set = fix_filtered[1]
-    alg_fix_count = length(fix_count_set[,1])
-    alg_fix_dur_avg = mean(fix_filtered$Duration, na.rm = T)
+    fix_count_set_gaze = fix_filtered_gaze[1]
+    alg_fix_count_gaze = length(fix_count_set_gaze[,1])
+    alg_fix_dur_avg_gaze = mean(fix_filtered_gaze$Duration, na.rm = T)
     
     # #Getting saccade metrics:
-    sac_filtered = s_gaze%>%
+    sac_filtered_gaze = s_gaze%>%
       filter(Value == "s")
     
-    sac_count_set = sac_filtered[1]
-    alg_sac_count = length(sac_count_set[,1])
-    alg_sac_dur_avg = mean(sac_filtered$Duration, na.rm = T)
+    sac_count_set_gaze = sac_filtered_gaze[1]
+    alg_sac_count_gaze = length(sac_count_set_gaze[,1])
+    alg_sac_dur_avg_gaze = mean(sac_filtered_gaze$Duration, na.rm = T) / 10
     
-    # #adding to averaging data set
-    alg_data = c(alg_fix_count, alg_fix_dur_avg, alg_sac_count, alg_sac_dur_avg)
+    #DISPERSION
+    alg_results_dis = gazepath(eye_data, x1 = 8, y1 = 9, d1 = 12, trial = 11, height_mm = screen[1,2],
+                                width_mm = screen[2,2], width_px = screen[3,2], height_px = screen[4,2], method = "dispersion",
+                                samplerate = 1000, thres_dur = 80)
+    s_dis = summary(alg_results_dis)
+    
+    #Getting fixation metrics:
+    fix_filtered_dis = s_dis%>%
+      filter(Value == "f")
+    
+    fix_count_set_dis = fix_filtered_dis[1]
+    alg_fix_count_dis = length(fix_count_set_dis[,1])
+    alg_fix_dur_avg_dis = mean(fix_filtered_dis$Duration, na.rm = T)
+    
+    # #Getting saccade metrics:
+    sac_filtered_dis = s_dis%>%
+      filter(Value == "s")
+    
+    sac_count_set_dis = sac_filtered_dis[1]
+    alg_sac_count_dis = length(sac_count_set_dis[,1])
+    alg_sac_dur_avg_dis = mean(sac_filtered_dis$Duration, na.rm = T) / 10
+    
+    #VELOCITY
+    
+    alg_results_vel = gazepath(eye_data, x1 = 8, y1 = 9, d1 = 12, trial = 11, height_mm = screen[1,2],
+                                width_mm = screen[2,2], width_px = screen[3,2], height_px = screen[4,2], method = "velocity",
+                                samplerate = 1000, thres_dur = 80)
+    s_vel = summary(alg_results_vel)
+    
+    #Getting fixation metrics:
+    fix_filtered_vel = s_vel%>%
+      filter(Value == "f")
+    
+    fix_count_set_vel = fix_filtered_vel[1]
+    alg_fix_count_vel = length(fix_count_set_vel[,1])
+    alg_fix_dur_avg_vel = mean(fix_filtered_vel$Duration, na.rm = T)
+    
+    # #Getting saccade metrics:
+    sac_filtered_vel = s_vel%>%
+      filter(Value == "s")
+    
+    sac_count_set_vel = sac_filtered_vel[1]
+    alg_sac_count_vel = length(sac_count_set_vel[,1])
+    alg_sac_dur_avg_vel = mean(sac_filtered_vel$Duration, na.rm = T)
+    
+    #adding to averaging data set
+    alg_data = c(alg_fix_count_gaze, alg_fix_count_dis, alg_fix_count_vel,
+                 alg_fix_dur_avg_gaze, alg_fix_dur_avg_dis, alg_fix_dur_avg_vel,
+                 alg_sac_count_gaze, alg_sac_count_dis, alg_sac_count_vel,
+                 alg_sac_dur_avg_gaze, alg_sac_dur_avg_dis, alg_sac_dur_avg_vel)
     
     #Getting the subject's fixation and saccade groundtruth values
     
@@ -85,15 +134,14 @@ for (i in 1:length(event_folders)) {
     file_list_fix = list.files(getwd(), pattern="*.csv")
     
     #Choose the respective folder that matches the subject from the algorithm 
-    fix_file = file_list_fix[(l+1)/2]
+    fix_file = file_list_fix[(l+1)/2] # change index to l if dominant data
     if(substr(alg_file,1,length(alg_file)-4) != substr(fix_file,1,length(alg_file)-4)){
       stop(c(alg_file,fix_file))
     }
-    print(c(alg_file,fix_file))
     eye_data_fix = read.csv(fix_file, header = FALSE)
     fix_count = length(eye_data_fix[,1])
     fix_dur_avg = mean(eye_data_fix[,6], na.rm = T)
-    ground_data = c(fix_count, fix_dur_avg)
+    ground_data = c(rep(fix_count,3), rep(fix_dur_avg,3))
     
     #Saccade
     #Get to the right folder of 30 csvs
@@ -107,22 +155,21 @@ for (i in 1:length(event_folders)) {
     file_list_sac = list.files(getwd(), pattern="*.csv");
     
     #Choose the respective folder that matches the subject from the algorithm
-    sac_file = file_list_sac[(l+1)/2]
+    sac_file = file_list_sac[(l+1)/2] # change index to l if dominant data
     if(substr(alg_file,1,length(alg_file)-4) != substr(sac_file,1,length(alg_file)-4)){
       stop(c(alg_file,sac_file))
     }
-    print(c(alg_file,sac_file))
+ 
     eye_data_sac = read.csv(sac_file, header = FALSE)
     sac_count = length(eye_data_sac[,1])
     sac_dur_avg = mean(eye_data_sac[,6], na.rm = T)
-    ground_data = c(ground_data, sac_count, sac_dur_avg)
+    ground_data = c(ground_data, rep(sac_count,3), rep(sac_dur_avg,3))
     
     
     #Comparing algorithm and groundtruth values
     squared_differences = (ground_data - alg_data)^2
     
     SD_event_data = rbind(SD_event_data, squared_differences) #30 values of each subject's squared diff values for four metrics 
-    names(SD_event_data) = c("SD fixation count", "SD fixation duration", "SD saccade count", "SD saccade duration") 
     
     setwd(event_path)
     
@@ -132,7 +179,7 @@ for (i in 1:length(event_folders)) {
   
   RMSE_values = ext1
   
-  for (i in 1:4) {
+  for (i in 1:12) {
     RMSE_value = RMSE(SD_event_data, i)
     RMSE_values = c(RMSE_values, RMSE_value)
   }
@@ -146,6 +193,9 @@ for (i in 1:length(event_folders)) {
   setwd(main_path)
 }
 
-names(RMSE_data) <- c("Filename", "RMSE fixation count", "RMSE fixation duration", "RMSE saccade count", "RMSE saccade duration") 
+names(RMSE_data) <- c("Filename", "RMSE gazepath fixation count", "RMSE dispersion fixation count", "RMSE velocity fixation count",
+                      "RMSE gazepath fixation duration", "RMSE dispersion fixation duration", "RMSE velocity fixation duration",
+                      "RMSE gazepath saccade count", "RMSE dispersion saccade count", "RMSE velocity saccade count",
+                      "RMSE gazepath saccade duration", "RMSE dispersion saccade duration", "RMSE velocity saccade duration") 
 
-write.csv(RMSE_data,"/Users/Tarsus/Desktop/RMSE/test.csv", row.names = FALSE) #change this to your where you want to output the csv with the same title
+write.csv(RMSE_data,"/Users/Tarsus/Desktop/Data/RMSE/test.csv", row.names = FALSE) #change this to your where you want to output the csv with the same title
